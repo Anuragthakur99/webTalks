455e13994ae9a55ab84c8d354b4466f6370f54ba
Anuragthakur99 <anurag.thkr009@gmail.com>
2024-12-12 03:29:51 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,16 +16,29 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+const allowedOrigins = [
+  "http://localhost:5173", // For local development
+  "https://webtalks-frontend.onrender.com", // For deployed frontend
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-app.use(express.json({
-  limit: "50mb",
 
-}));
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
@@ -41,6 +54,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("Server is running on PORT:", PORT);
   connectDB();
 });
